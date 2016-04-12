eaa2f5308385992dd2514a4d6b3aa0b98ebf8daa
Ben Brew <benmbrew@gmail.com>
2016-04-12 16:42:47 -0400

    added time

--- rome/rome_code.R
+++ rome/rome_code.R
@@ -12,12 +12,15 @@ trafficFolder <- paste0(projectFolder, '/rome/car_accidents')
 
 # Load raw footballa
 football <- read.csv(paste0(footballFolder, '/as_roma_games.csv'), header = FALSE)
+football_time <- read.csv(paste0(footballFolder, '/as_roma_times.csv'), header = FALSE)
 
 # git rid of unnecssary columns
 football <- football[, c(1:4,9)]
+football_time <- football_time[, 1:4]
 
 # rename columns
 colnames(football) <- c('date', 'home_team', 'score', 'away_team', 'outcome')
+colnames(football_time) <- c('date', 'time', 'home', 'other_team')
 
 # create indicator for home or away
 football$home <- ifelse(football$home_team == 'AS Roma', TRUE, FALSE)
@@ -26,11 +29,19 @@ football$home <- ifelse(football$home_team == 'AS Roma', TRUE, FALSE)
 football$date <- parse_date_time(x = football$date,
                             orders = c("d m y", "d B Y", "m/d/y"))
 football$date <- as.Date(football$date, format = 'Y%-%m-%d')
+football_time$date <- as.Date(football_time$date, format = '%d/%m/%Y')
+football_time$time <- paste0(football_time$time, ':00')
 
 # remove unneeded columns
 football$home_team <- NULL
 football$away_team <- NULL
 football$score <- NULL
+football_time$home <- NULL
+football_time$other_team <- NULL
+
+# join football and football_time
+football <- left_join(football, football_time,
+                      by = 'date')
 
 # organize football so it matches accidents 
 football <- football %>%
@@ -38,10 +49,9 @@ football <- football %>%
          location_bi = ifelse(home, 'home', 'away'),
          outcome = ifelse(outcome == 'L', 'loss',
                           ifelse(outcome == 'W', 'win', 'tie')),
+         time = time,
          score = NA,
          location = NA) 
-
-
 
 ##################################################################################
 # Read in rome traffic data 
@@ -54,14 +64,20 @@ accidents <- rbind(jan_june_2013, july_dec_2013, july_dec_2014)
 
 # convert date 
 temp <- strsplit(as.character(accidents$DataOraIncidente), " ")
-temp.2 <- lapply(temp, function(x) x[1])
-temp.3 <- unlist(cbind(temp.2))
-accidents$DataOraIncidente <- as.factor(temp.3)
+temp.date <- lapply(temp, function(x) x[1])
+temp.time <- lapply(temp, function(x) x[2])
+temp.date <- unlist(cbind(temp.date))
+temp.time <- unlist(cbind(temp.time))
+accidents$DataOraIncidente <- as.factor(temp.date)
 accidents$DataOraIncidente <- as.Date(accidents$DataOraIncidente, format = '%d/%m/%Y')
+accidents$time <- as.factor(temp.time)
+accidents$time <- gsub('.', ':', accidents$time, fixed = TRUE)
+
 
 # group by dates and get counts for accidents
 accidents <- accidents %>%
-  group_by(date = DataOraIncidente) %>%
+  group_by(date = DataOraIncidente,
+           accident_time = time) %>%
   summarise(accidents = n())
 
 # Join accidents and football
@@ -76,8 +92,8 @@ rome <-
   left_join(accidents, by = 'date') %>%
   mutate(game_day = ifelse(is.na(game_day), FALSE, game_day),
          dataset = 'rome') %>%
-  dplyr::select(date, game_day, location_bi, outcome, score,
+  dplyr::select(date, time, accident_time,game_day, location_bi, outcome, score,
                 location, dataset, accidents)
 
 # Get rid of anything extra
-rm(accidents, football, jan_june_2013, july_dec_2013, july_dec_2014)
+rm(accidents, football, football_time, jan_june_2013, july_dec_2013, july_dec_2014)
